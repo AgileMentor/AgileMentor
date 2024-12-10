4a562d2921dd14cc5c96ad94008a982bc26a037c
HyeonJi05 <eockdwndbth@naver.com>
2024-12-10 18:06:39 +0900

    feat: 스프린트 수정

--- backend/src/main/java/agilementor/sprint/repository/SprintRepository.java
+++ backend/src/main/java/agilementor/sprint/repository/SprintRepository.java
@@ -2,19 +2,13 @@ package agilementor.sprint.repository;
 
 import agilementor.project.entity.Project;
 import agilementor.sprint.entity.Sprint;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SprintRepository extends JpaRepository<Sprint, Long> {
-
-    // 특정 프로젝트의 모든 스프린트 조회
-    List<Sprint> findByProject_ProjectId(Long projectId);
 
     // 특정 프로젝트와 특정 스프린트 ID 조회
     Optional<Sprint> findByProject_ProjectIdAndId(Long projectId, Long sprintId);
@@ -22,21 +16,11 @@ public interface SprintRepository extends JpaRepository<Sprint, Long> {
     // 프로젝트 ID로 스프린트 개수를 카운트
     long countByProject_ProjectId(Long projectId);
 
-    // 특정 스프린트 조회
-    Optional<Sprint> findByIdAndProject(Long id, Project project);
-
     // 활성 스프린트 조회
     Optional<Sprint> findByProjectAndIsActivateTrue(Project project);
 
     // 특정 프로젝트의 isdone이 false인 모든 스프린트 조회
     List<Sprint> findByProject_ProjectIdAndIsDoneFalse(Long projectId);
-
-    // 추가된 메서드
-    List<Sprint> findByProject_ProjectIdAndIsDoneTrue(Long projectId);
-
-    @Query("SELECT s FROM Sprint s WHERE s.project.projectId = :projectId AND (s.isActivate = true OR s.endDate = :today)")
-    Optional<Sprint> findActiveOrEndingSprint(@Param("projectId") Long projectId,
-        @Param("today") LocalDate today);
 
     List<Sprint> findByProject_ProjectIdAndIsDoneTrueOrderByEndDateAsc(Long projectId);
 }
