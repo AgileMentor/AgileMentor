c2240133ccd092d3842b5f1318c32f4c246242dc
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-12-15 03:36:10 +0900

    refactor: enum들의 불필요한 상수 제거

--- backend/src/main/java/agilementor/backlog/entity/Priority.java
+++ backend/src/main/java/agilementor/backlog/entity/Priority.java
@@ -1,17 +1,7 @@
 package agilementor.backlog.entity;
 
 public enum Priority {
-    HIGH("HIGH"),
-    MEDIUM("MEDIUM"),
-    LOW("LOW");
-
-    private final String value;
-
-    Priority(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    HIGH,
+    MEDIUM,
+    LOW
 }
