44fd4bec8fa61ab50f2ed175a416bc666a9755cf
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-12-15 14:47:15 +0900

    feat: LoginMemberIdArgumentResolver 구현

--- backend/src/main/java/agilementor/common/config/WebConfig.java
+++ backend/src/main/java/agilementor/common/config/WebConfig.java
@@ -1,11 +1,20 @@
 package agilementor.common.config;
 
+import agilementor.common.resolver.LoginMemberIdArgumentResolver;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginMemberIdArgumentResolver loginMemberIdArgumentResolver;
+
+    public WebConfig(LoginMemberIdArgumentResolver loginMemberIdArgumentResolver) {
+        this.loginMemberIdArgumentResolver = loginMemberIdArgumentResolver;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -16,4 +25,10 @@ public class WebConfig implements WebMvcConfigurer {
             .maxAge(1800)
             .allowCredentials(true);
     }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberIdArgumentResolver);
+    }
+
 }
